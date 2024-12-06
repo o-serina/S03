42af4d4eb763013dd17c71d39efaf071b2f7359c
o-serina <so88.opy@gmail.com>
2024-12-06 04:05:58 -0500

    styles and content added

--- S03-angular/src/app/app-routing.module.ts
+++ S03-angular/src/app/app-routing.module.ts
@@ -3,30 +3,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { NavComponent } from './nav/nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeaderComponent } from './header/header.component';
 import { SummaryComponent } from './summary/summary.component';
 import { ReportsComponent } from './reports/reports.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: '',
-    component: NavComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'summary', component: SummaryComponent },
-      { path: 'reports', component: ReportsComponent }
-    ],
-  },
-  { path: 'header', component: HeaderComponent },
-  { path: '**', redirectTo: 'login' }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirect to 'dashboard' as the default route
+  { path: '**', redirectTo: 'login' } // Catch-all redirect to 'login' for unmatched paths
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
